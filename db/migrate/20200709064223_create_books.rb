df4e44f00cdb2b8f44cb760f4735b541f169ea77
ykitahara-cq <y.kitahara@caraquri.com>
2020-07-09 16:20:13 +0900

    excute migrate

---
+++ db/migrate/20200709064223_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :name, null: false 
+      t.string :image, null: false
+      t.integer :price, null: false
+      t.string :purchase_date, null: false
+
+      t.timestamps
+    end
+  end
+end
