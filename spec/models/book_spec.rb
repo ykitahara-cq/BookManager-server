68067014663ccdca773e1ac091aa818aa61fc5c9
ykitahara-cq <y.kitahara@caraquri.com>
2020-08-07 11:04:48 +0900

    delete comment & needless test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   describe 'validatation' do 
     context '存在チェック' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   let(:invalid_user) {build(:user, :invalid_user)}
   describe 'validatation' do 
     context '存在チェック' do
